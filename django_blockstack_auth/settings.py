f6f7ce1df0105f09be5cbe67dd618030f1e16e18
Mark Steadman <mark@bloomsbury.digital>
2017-12-07 16:20:41 +0000

    Adds Blockstack app settings

---
+++ django_blockstack_auth/settings.py
@@ -0,0 +1,18 @@
+from django.conf import settings as site_settings
+
+
+APP_NAME = getattr(
+    site_settings, 'BLOCKSTACK_APP_NAME', 'Hello Blockstack'
+)
+
+APP_DESCRIPTION = getattr(
+    site_settings,
+    'BLOCKSTACK_APP_DESCRIPTION',
+    'A simple demo of Blockstack Auth'
+)
+
+APP_ICON = (site_settings.STATIC_ROOT or '/static/') + getattr(
+    site_settings,
+    'BLOCKSTACK_APP_ICON',
+    'blockstack/img/icon-192x192.png'
+)
