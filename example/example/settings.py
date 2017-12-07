882e077d07640f4f85de649ef09402d79c4b22f4
Mark Steadman <mark@bloomsbury.digital>
2017-12-07 21:12:44 +0000

    Adds the Blockstack authentication backend to the example app

--- example/example/settings.py
+++ example/example/settings.py
@@ -35,11 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'django_blockstack_auth',
-
-    # if your app has other dependencies that need to be added to the site
-    # they should be added here
+    'django_blockstack_auth'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -101,20 +97,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django_blockstack_auth.backends.BlockstackBackend',
+    'django.contrib.auth.backends.ModelBackend'
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/blockstack/manifest.json'
